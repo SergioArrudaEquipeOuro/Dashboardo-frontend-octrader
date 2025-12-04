8abae3a21bad6a0c3963c8475bed72f657b20df0
SergioArrudaEquipeOuro <equipeouro85@gmail.com>
2025-12-04 09:11:32 -0300

    att

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConnectionService {
-  api = 'http://localhost:8080/'
-  /* api = 'https://ooctrader-8b6042370daf.herokuapp.com/' */
+  /* api = 'http://localhost:8080/' */
+  api = 'https://ooctrader-8b6042370daf.herokuapp.com/'
 
   constructor() { }
 
